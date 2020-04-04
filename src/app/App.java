0a00214c0dfc80910313203ee6cc11433fd0528e
JeSSich <schwabjessica@gmail.com>
2020-04-04 12:57:57 -0300

    Modifique nombres se clases

--- src/app/App.java
+++ src/app/App.java
@@ -1,30 +1,59 @@
 package app;
-
+import java.util.Scanner;
 public class App {
+    public static Scanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-    
-       
-            Poolfix miPooflix = new Poolfix();
-    
-            System.out.println("Iniciando Catalogo");
-            
-            miPooflix.inicializarCatalogo();
-    
-            Serie bb = miPooflix.buscarSerie("Breaking Bad");
-            
-        
-            Temporada laMejorTemporada = bb.getTemporada(5);
-            
-            Episodio elTop;
-            
-            elTop = laMejorTemporada.getEpisodio(7);
-            
-            elTop.reproducir();
 
-            
-      
+        Poolfix miPooflix = new Poolfix();
 
+        System.out.println("Iniciando Catalogo....");
 
+        miPooflix.inicializarCatalogo();
+
+        Serie bb = miPooflix.buscarSerie("Walking Dead");
+
+        Temporada laMejorTemporada = bb.getTemporada(4);
+
+        Episodio epiDemo;
+
+        int posicion = 0;
+
+        /// En base a un numero de posicion de episodio, obtenerlo de la lista
+        System.out.println("Ingrese la posicion");
+
+        posicion = Teclado.nextInt();
+
+        epiDemo = laMejorTemporada.getEpisodioAtPosicion(posicion);
+
+        epiDemo.reproducir();
+
+        // instanceof: devuelve si la variable es de tipo Clase X
+        // En este caso, devuelve si epiDemo declarado como Episodio apunta a una clase
+        // Websodio
+        if (epiDemo instanceof Webisodios) {
+            System.out.println("Estoy reproduciendo un websodio");
+
+            // (clase): Operador casteo.
+            // Casteo lo que hace es acomodar la variable a castear, a una clase especifica
+            // da error si no se puede acomodar. por eso siempre se pregunta antes con un
+            // instance of
+            Webisodios webso = (Webisodios) epiDemo;
+            webso.enviarAlertaDeQueEstaOnline();
+        } else
+            System.out.println("Estoy reproduciendo un Episodio");
+
+        Episodio elTop;
+
+        elTop = laMejorTemporada.getEpisodio(2);
+
+        elTop.reproducir();
+
+        Episodio otroEpi;
+
+        otroEpi = laMejorTemporada.getEpisodio(2);
+
+        otroEpi.reproducir();
 
 
 
--- src/app/Poolfix.java
+++ src/app/Poolfix.java
@@ -85,80 +85,75 @@ public class Poolfix {
     }
 
 
-
-    public void agregarDoctorHouse() {
-
-        Serie doctorH = new Serie();
-        doctorH.titulo= "Doctor House";
-     
-        Genero genero = new Genero();
-        genero.nombre = "Ficcion";
-     
-        Episodio episodio = new Episodio();
-        episodio.nombre ="episodio 3";
-        episodio.duracion =1.5;
-        episodio.numero =3; 
-     
-        Temporada t5 = new Temporada();
-        t5.numero = 2;
-     
-        Episodio ep = new Episodio();
-     
-        ep.numero = 7;
-        ep.nombre = "El dolor";
-        ep.duracion = 43;
-     
-        t5.episodios.add(ep);
-     
-        Episodio epOtro = new Episodio();
-     
-        epOtro.numero = 5;
-        epOtro.nombre = "Como curarse";
-        epOtro.duracion = 41;
-     
-        t5.episodios.add(epOtro);
-     
-        doctorH.temporadas.add(t5);
-     
-        this.series.add(doctorH); 
-       
-     
-         }
-     
+   
     public void inicializarCatalogo() {
 
-        /* Serie breakingB = new Serie();
+        Serie breakingB = new Serie();
         breakingB.titulo = "Breaking Bad";
-
         Genero genero = new Genero();
         genero.nombre = "Drama";
         breakingB.generos.add(genero);
 
         Temporada t5 = new Temporada();
         t5.numero = 5;
-
         Episodio ep = new Episodio();
-
         ep.numero = 7;
         ep.nombre = "Say my name";
         ep.duracion = 43;
-
         t5.episodios.add(ep);
-
         Episodio epOtro = new Episodio();
-
         epOtro.numero = 5;
         epOtro.nombre = "Ozymandias";
         epOtro.duracion = 41;
-
         t5.episodios.add(epOtro);
-
         breakingB.temporadas.add(t5);
+        this.series.add(breakingB); 
 
-        this.series.add(breakingB); */
+        Serie walkingDeadSerie = new Serie();
+        walkingDeadSerie.titulo = "Walking Dead";
 
-        this.agregarBreakingBad();
-        this.agregarDoctorHouse();
+        Temporada t3 = new Temporada();
+        t3.numero=3;
+        Webisodios webisodios= new Webisodios();
+        webisodios.numero = 3 ;
+        webisodios.nombre = "Alone ";
+        webisodios.duracion = 10;
+        webisodios.url="http//ladire.com";
+        t3.episodios.add(webisodios);
+        walkingDeadSerie.temporadas.add(t3);
+
+        Episodio epi8 = new Episodio();
+        Temporada t4 = new Temporada();
+        t4.numero = 4;
+        epi8.numero = 8;
+        epi8.nombre = "Too Far Gone";
+        epi8.duracion = 43;
+        t4.episodios.add(epi8);
+
+        Episodio epi9 = new Episodio();
+        epi9.numero = 9;
+        epi9.nombre = "After";
+        epi9.duracion = 43;
+        t4.episodios.add(epi9);
+        walkingDeadSerie.temporadas.add(t4);
+       
+       
+        Temporada t7 = new Temporada();
+        t7.numero = 7;
+        Episodio epi7 = new Episodio();
+        epi7.numero = 1;
+        epi7.nombre = "The Day Will Come When You Won't Be";
+        epi7.duracion = 45;
+        t7.episodios.add(epi7);
+
+        Episodio epi3 = new Episodio();
+        epi3.numero = 3;
+        epi3.nombre = "The Cell";
+        epi3.duracion = 43;
+        t7.episodios.add(epi3);
+        walkingDeadSerie.temporadas.add(t7);
+        
+        this.series.add(walkingDeadSerie); 
 
     }
 
